14de4591f516880d8892e5dac12c6d0943297b23
Mateja Lasan <m.vuk.lasan@icloud.com>
2021-06-13 14:14:01 -0400

    Strip unnecessary components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
-import { UserProvider } from '@auth0/nextjs-auth0'
 import { AppProps } from "next/app"
 import 'styles/index.css'
 
 export default function Website({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <Component {...pageProps} />
   )
 }
