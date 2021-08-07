8ca0825393aeaf278b60b24c943add4532ece649
Mateja Lasan <m.vuk.lasan@icloud.com>
2021-08-07 13:32:08 -0400

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from "next/app"
+import Script from "next/script"
 import "@styles/index.css"
 
 export default function Website({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <Script
+      src="https://scripts.simpleanalyticscdn.com/latest.js"
+      strategy="afterInteractive"
+    />
+  </>
 }
